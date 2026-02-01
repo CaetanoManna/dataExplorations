eb05adb820547a35c9d4fd883d367275d121e8e1
CaetanoManna <caetano.manna@gmail.com>
2026-02-01 01:05:17 -0300

    Mudando para season 2019-2024

--- src/extract/capture_games.py
+++ src/extract/capture_games.py
@@ -22,37 +22,40 @@ def fetch_games_2024():
     season = config.get("nba_api.season")
     league_id = config.get("nba_api.league_id")
     season_type = config.get("nba_api.season_type")
+    for i in season:
 
-    gamefinder = leaguegamefinder.LeagueGameFinder(
-        season_nullable=season,
-        league_id_nullable=league_id,
-        season_type_nullable=season_type
-    )
+        gamefinder = leaguegamefinder.LeagueGameFinder(
+            season_nullable=i,
+            league_id_nullable=league_id,
+            season_type_nullable=season_type
+        )
 
-    games = gamefinder.get_data_frames()[0]
+        games = gamefinder.get_data_frames()[0]
 
-    games.columns = games.columns.str.lower()
-
-    OUTPUT_PATH.parent.mkdir(parents=True, exist_ok=True)
-    games.to_csv(OUTPUT_PATH, index=False)
+        games.columns = games.columns.str.lower()
+    
+        OUTPUT_PATH.parent.mkdir(parents=True, exist_ok=True)
+        games.to_csv(OUTPUT_PATH, index=False, mode='a')
 
     logger.info(f"Saved raw data to {OUTPUT_PATH}")
 
 def capture_players():
-    from nba_api.stats.endpoints import LeagueGameLog
 
-    gamelog = LeagueGameLog(
-        season="2023-24",
-        season_type_all_star="Regular Season",
-        player_or_team_abbreviation="P"
-    )
+    season = config.get("nba_api.season")
+    season_type = config.get("nba_api.season_type")
 
-    df = gamelog.get_data_frames()[0]
+    for i in season:
+        gamelog = LeagueGameLog(
+                season= i,
+                season_type_all_star= season_type,
+                player_or_team_abbreviation="P"
+            )
+        df = gamelog.get_data_frames()[0]
 
-    df.columns = df.columns.str.lower()
+        df.columns = df.columns.str.lower()
 
-    OUTPUT_PATH_PLAYERS.parent.mkdir(parents=True, exist_ok=True)
-    df.to_csv(OUTPUT_PATH_PLAYERS, index=False)
+        OUTPUT_PATH_PLAYERS.parent.mkdir(parents=True, exist_ok=True)
+        df.to_csv(OUTPUT_PATH_PLAYERS, index=False, mode='a')
 
     logger.info(f"Saved raw players to {OUTPUT_PATH_PLAYERS}")
 
